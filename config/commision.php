3e57a1443d45de101a01dff7fe44c5896daf1986
Dominykas <dominykas.rakunas@presentconnection.eu>
2021-08-29 20:10:30 +0300

    config cleanup

--- config/commision.php
+++ config/commision.php
@@ -5,7 +5,6 @@ return [
     'withdrawCommissionBusiness' => 0.005,
     'withdrawPrivateFreeTransactions' => 3,
     'withdrawCommissionPrivateDiscount' => 1000,
-    'depositCommissionLimitMax' => 3,
     'depositCommissionPercent' => 0.003,
     'withdrawCommissionBusinessLimit' => 0.5,
 ];
